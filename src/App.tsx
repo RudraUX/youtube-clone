a2af959ea4553750b169d4d9775714a5d90815eb
RudraUX <pollabrock@gmail.com>
2023-01-22 01:14:12 +0600

    project setup and pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import {
+  Navbar,
+  Feed,
+  ChannelDetail,
+  SearchFeed,
+  VideoDetail,
+} from './components';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function increment() {
-    setCount(count + 1);
-  }
-
   return (
-    <div className='App'>
-      {count}
-      <button onClick={increment}>Add</button>
-    </div>
+    <BrowserRouter>
+      <Box sx={{ background: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
