9caa57ef443e60246d3d50731474f9de4180b635
RudraUX <pollabrock@gmail.com>
2023-01-29 17:44:17 +0600

    searchbar added

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Typography, Stack } from '@mui/material';
 
 function Feed() {
-  return <div>Feed</div>;
+  return (
+    <Stack
+      sx={{
+        flexDirection: {
+          xs: 'column',
+          md: 'row',
+        },
+      }}
+    >
+      <Box
+        sx={{
+          height: {
+            xs: 'auto',
+            md: '100vh',
+          },
+        }}
+      ></Box>
+    </Stack>
+  );
 }
 
 export default Feed;
