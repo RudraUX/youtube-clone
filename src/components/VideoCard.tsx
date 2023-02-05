f3c3e16780bc2f112c169a3ffa71198861904ec2
RudraUX <pollabrock@gmail.com>
2023-02-05 15:55:16 +0600

    ✨videocard component added

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
@@ -10,10 +9,48 @@ import {
   demoChannelUrl,
   demoChannelTitle,
 } from '../utils/constants';
+import { height } from '@mui/system';
 
-function VideoCard({ video: { id: videoId } }: { video: any }) {
-  console.log(videoId);
-  return <div>VideoCard</div>;
+function VideoCard({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}: {
+  video: any;
+}) {
+  console.log(snippet);
+  return (
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
+          component='img'
+          alt={snippet?.title}
+          sx={{ height: 180, width: 358 }}
+        />
+        <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+          <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+            <Typography variant='subtitle1' fontWeight='bold' color='#fff'>
+              {snippet?.title.slice(0, 50) || demoVideoTitle.slice(0, 50)}
+            </Typography>
+          </Link>
+          <Link
+            to={
+              snippet?.channelId
+                ? `/video/${snippet?.channelId}`
+                : demoChannelUrl
+            }
+          >
+            <Typography variant='subtitle1' fontWeight='bold' color='gray'>
+              {snippet?.channelTitle || demoChannelTitle}
+              <CheckCircle sx={{ color: '#fff', fontSize: 'small' }} />
+            </Typography>
+          </Link>
+        </CardContent>
+      </Link>
+    </Card>
+  );
 }
 
 export default VideoCard;
