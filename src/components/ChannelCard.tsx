ea012358a22183d55221d36cb4d56b85f900ad93
RudraUX <pollabrock@gmail.com>
2023-02-07 18:34:44 +0600

    ✨api key changed

--- src/components/ChannelCard.tsx
+++ src/components/ChannelCard.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import { Box, CardContent, CardMedia, Typography } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { demoProfilePicture } from '../utils/constants';
 
 function ChannelCard({ channelDetail }: { channelDetail: any }) {
-  return <div>ChannelCard</div>;
+  return (
+    <Box sx={{ boxShadow: 'none', borderRadius: '20px' }}>
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            textAlign: 'center',
+            color: 'white',
+          }}
+        >
+          <CardMedia
+            image={
+              channelDetail?.snippet?.thumbnails?.high?.url ||
+              demoProfilePicture
+            }
+            component='img'
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              height: 180,
+              width: 180,
+              borderRadius: '50%',
+              mb: 2,
+              border: '2px solid #e3e3e3',
+            }}
+          />
+          <Typography variant='h6' fontWeight='bold'>
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ color: 'gray', fontSize: '14px', ml: '5px' }} />
+          </Typography>
+        </CardContent>
+      </Link>
+    </Box>
+  );
 }
 
 export default ChannelCard;
